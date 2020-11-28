2beff252db6c94f94decb06b88467c9eb296a4b2
igor-turcan <iturcan@student.rtc.edu>
2020-11-27 21:34:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # This is the template code for the CNA337 Final Project
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
-
+# Igor Turcan
+# Tutoring help: Daniel Turcan, dan.tur000@gmail.com
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Igor")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    import os
+    ip = "18.224.151.54"
+    response = os.system("ping -n 4 " + ip)
     # TODO - Call Ping method and print the results
+    if response == 0:
+        print(ip, 'is up!')
+    else:
+        print(ip, 'is down!')
