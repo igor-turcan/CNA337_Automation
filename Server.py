4385d8f8746338e4a80a241e8410ed3573666bf8
igor-turcan <iturcan@student.rtc.edu>
2020-12-04 19:54:42 -0800

    SSH Update

--- Server.py
+++ Server.py
@@ -1,13 +1,76 @@
-# Pinging AWS EC2 public IPv4.
+# CNA 337 Fall 2020
+# Automate SSH
+# Participants: Igor Turcan, Vlado Situm, Dorin Vozian, Abdirizak Kulmie
+# Tutoring Liviu Patrasco liviu_patrasco@hotmail.com
 import os
+import paramiko
+
 
 class Server:
     """ Server class for representing and manipulating servers. """
 
-    def __init__(self, server_ip):
+    def __init__(self, server_ip, key_file, username, upgrade_command):
         # TODO -
+
+        self.username = username
+        self.command = upgrade_command
+        self.key_file = key_file
         self.server_ip = server_ip
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return os.system("ping -n 4 %s" % self.server_ip)
+        result = os.system("ping -n 4 %s" % self.server_ip)
+        return result
+
+    def upgrade(self):
+        # TODO - Use os module to ping the server
+
+        # Create the ssh client
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        # Make the connection to the server
+        ssh_client.connect(hostname=self.server_ip, username=self.username, key_filename=self.key_file)
+
+        # execute the command
+        stdin, stdout, stderr = ssh_client.exec_command(self.command)
+
+        # Return the result from both stdout and error
+        result = stdout.readlines() + stderr.readlines()
+
+        # Disconnect
+        ssh_client.close()
+
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,22 +1,62 @@
-# CNA337 Final Project
-# Zachary Rubin, zrubin@rtc.edu
-# Group participans:  Dorin,  Saeid, Vlado, Mohammad and Abdy.
-# Tutoring help: Liviu Patrisco, liviu_patrisco@hotmail.com
-
+# CNA 337 Fall 2020
+# Automate SSH
+# Participants: Igor Turcan, Vlado Situm, Dorin Vozian, Abdirizak Kulmie
+# Tutoring Liviu Patrasco liviu_patrasco@hotmail.com
 from Server import Server
+
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Igor")
+    print("Server Automator v0.1 by Igor Turcan")
+
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    ec2_ip_address = "18.224.151.54"
-    server = Server(ec2_ip_address)
-    result = server.ping()
-    print(result)
-    if result == 0:
-        print("Server with ip [%s] is up." % ec2_ip_address)
+    # TODO - Create a Server object
+    # TODO - Call Ping method and print the results
+
+    my_server_ip = "18.224.151.54"
+    my_rsa_key_file = "C:\\Users\\dantu\\.ssh\\id_rsa"
+    username = "ubuntu"
+
+    my_upgrade_command = 'sudo apt update && sudo apt upgrade -y'
+    my_server = Server(my_server_ip, my_rsa_key_file, username, my_upgrade_command)
+
+    print('Pinging server %s...' % my_server_ip)
+    ping_result = my_server.ping()
+    print(ping_result)
+    if ping_result == 0:
+        print("Pinging IP [%s] successful." % my_server_ip)
     else:
-        print("Server with ip [%s] is down." % ec2_ip_address)
+        print("Pinging IP [%s] Failed." % my_server_ip)
+
+    print("\nUpdating server using ssh client from paramiko...")
+    ssh_result = my_server.upgrade()
+    print(''.join(ssh_result))
+
+    print('Done.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
